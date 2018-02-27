bfe91ab6ae922625415b0a73f242d2c744b378b4
paolaitu <paolaiturralde@gmail.com>
2018-02-26 19:05:13 -0600

    Final comments
    final

--- cachematrix.R
+++ cachematrix.R
@@ -16,3 +16,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
+## This function will execute the solve to get 
+## the matrix's inverse, it will validate if it's different to null to 
+## retrieve, if null, it will calculate the inverse, set the inverse and return it
+## The fact of returning a list() allows access to any other objects defined in the env. 
+## of the original function. The subsequent code can access the values of x or m, thought 
+## the use of getters and setters. We can also access like: myMatrix_object$getsolve()
+cacheSolve <- function(x, ...) {
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
+}
